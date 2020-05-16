5a5a87038536e568de359e009dfefbbdc3e30c38
RodolfoAP <rodolfoap@gmail.com>
2020-05-16 17:54:16 +0200

    Changes 2020/05/16-17:54:16
    Change-Id: I7545fe6ce2e3fe1f44970ca9089f5d293733a55b

--- 73-homography/app.cpp
+++ 73-homography/app.cpp
@@ -14,34 +14,26 @@ int main(int argc, char** argv) {
 	std::vector<Point2f> srcPoints;
 	std::vector<Point2f> dstPoints;
 
-// image points
+	// image points
 	srcPoints.push_back(Point2f( 29.,  36.));
 	srcPoints.push_back(Point2f(744.,  33.));
 	srcPoints.push_back(Point2f( 29., 545.));
 	srcPoints.push_back(Point2f(793., 490.));
 
-// src points are measured in cm
-	dstPoints.push_back(Point2f(  0., 100.));
-	dstPoints.push_back(Point2f(600.,   0.));
+	// src points are measured in cm
+	dstPoints.push_back(Point2f(000., 100.));
+	dstPoints.push_back(Point2f(600., 000.));
 	dstPoints.push_back(Point2f(200., 700.));
 	dstPoints.push_back(Point2f(600., 600.));
 
-	// Fill-in:
-	// Push all other points A, B, C,...,Z
-	// Fill-in: use the OpenCV findHomography to compute the homography matrix H.
 	// Take the third parameter as just 0, which means use regular method using all points.
 	Mat H=findHomography(srcPoints, dstPoints, 0);
-//.	cout << "The computed homography matrix size is: " << endl;
-//	cout << "rows: "<< H.rows << ", cols: "<< H.cols << endl;
-//	for(int i=0;i<H.rows; ++i){for(int j=0; j<H.cols;++j){cout<<H.at<float>(i,j)<<",\t";}cout<<endl;}
 	Mat Img=imread(argv[1],1);
 	Mat Out_Img;
-	// Fill-in:
-	// Use the warpPerspective method of OpenCV to convert the input image Img to
-	// the corrected image Out_Img of size 300x300 using H.
-	warpPerspective(Img, Out_Img, H, Size(600, 700));
-	imshow("Input", Img);
-	imshow("Output", Out_Img);
+
+	warpPerspective(Img, Out_Img, H, Size(700, 800));
+	imshow ("Input", Img);
+	imshow ("Output", Out_Img);
 	imwrite("output.png", Out_Img);
 
 	cv::FileStorage fs("output.yaml", cv::FileStorage::WRITE);
