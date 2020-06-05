84b8eec2be7b03a65c052b7f63a5ef9aff54df34
RodolfoAP <rodolfoap@gmail.com>
2020-06-06 00:13:45 +0200

    Changes 2020/06/06-00:13:45
    Change-Id: I9f0f1b3431f60dabc1eb185598cbcc488845f9b0

--- 78-sqlite/app.cpp
+++ 78-sqlite/app.cpp
@@ -6,28 +6,35 @@
 #define LOG std::cerr<<">>> "<<__FILE__<<"["<<__LINE__<<"]:"<<__func__<<"();"<<std::endl;
 typedef struct Register{
 	uint id;
-	char name[32];
+	std::string name;
 	int zone;
 	float score;
-	char state[32];
-	void print(){ printf("%d:%s:%d:%f:%s\n", id, name, zone, score, state); }
+	std::string state;
+	void print(){ printf("%d:%s:%d:%f:%s\n", id, name.c_str(), zone, score, state.c_str()); }
 } Register;
+void save(){
+	char data[4][64] {	"1927 Frank_Holdsworth 62 0.3272 Arizona",
+				"1862 Jim_Scofield 32 0.1323 Arkansas",
+				"6271 Thelonious_Grenadier 44 0.42864 Missouri",
+				"8261 Albert_Swallow 51 0.28674 Toronto"};
+	std::ofstream outFile;
+	outFile.open("table.data");
+	for(int i=0; i<4; ++i) outFile<<data[i]<<std::endl;
+	outFile.close();
+}
 int main() {
-	std::ifstream detectionFile;
-	std::string detLine;
-	char ch;
-	detectionFile.open("table.csv");
-	if(!detectionFile.is_open()) return 1;
-	std::vector<Register> regs;
-	while(getline(detectionFile, detLine)) {
-		std::istringstream ss;
+	save();
+	std::ifstream inFile;
+	inFile.open("table.data");
+	if(!inFile.is_open()) return 1;
+	std::string line;
+	while(getline(inFile, line)) {
 		Register r;
-		ss.str(detLine);
+		std::istringstream ss(line);
 		ss >> r.id >> r.name >> r.zone >> r.score >> r.state;
 		ss.str("");
-		regs.push_back(r);
 		r.print();
 	}
-	detectionFile.close();
+	inFile.close();
 	return 0;
 }
