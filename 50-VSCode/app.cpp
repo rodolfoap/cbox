70f81cdd8fda045064f4d6ad558452e7ed206126
RodolfoAP <rodolfo.alcazar@gtd.eu>
2020-06-07 13:39:06 +0200

    Changes 2020/06/07-13:39:06
    Change-Id: I4a3758e125502a72f612947c4a75f8c3a590b628

--- 50-VSCode/app.cpp
+++ 50-VSCode/app.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #define LOG std::cerr<<">>> "<<__FILE__<<"["<<__LINE__<<"]:"<<__func__<<"();"<<std::endl;
+#define log(text)(std::cerr<<text<<std::endl)
 
 int main() {
 	std::cerr<<"Hello, World!"<<std::endl;
