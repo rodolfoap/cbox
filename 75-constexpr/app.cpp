14f5d83d2d74c29aedb6d32a4f25e553871392b0
RodolfoAP <rodolfoap@gmail.com>
2020-06-11 20:55:37 +0200

    Changes 2020/06/11-20:55:37
    Change-Id: I7066683b9d31357a2dd575b804ce8cd98fe8601d

--- 75-constexpr/app.cpp
+++ 75-constexpr/app.cpp
@@ -1,4 +1,8 @@
 /*
+ * The constexpr specifier declares that it is possible
+ * to evaluate the value of the function or variable
+ * at compile time.
+ *
  * This is all compiled to a simple
  *
  *	movl	$13, %eax
