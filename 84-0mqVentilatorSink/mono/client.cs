43ca73e84996a696fcf0ade0a27a6106764f7b33
RodolfoAP <rodolfo.alcazar@gtd.eu>
2020-06-14 03:07:21 +0200

    Changes 2020/06/14-03:07:21
    Change-Id: Iec4efe61de04ef77507f081dac7cd4bd79e67ecf

--- 84-0mqVentilatorSink/mono/client.cs
+++ 84-0mqVentilatorSink/mono/client.cs
@@ -3,30 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading;
-
+using System.Text;
 using ZeroMQ;
 
 namespace Examples {
 static partial class Program {
 	public static void sender(){
-		while(true) {
-			Console.WriteLine("T...");
-			Thread.Sleep(1000);
+		using(var context=new ZContext()) using(var sockSend=new ZSocket(context, ZSocketType.PUSH)) {
+			Console.WriteLine("Sender: tcp://127.0.0.1:5558");
+			sockSend.Connect("tcp://127.0.0.1:5558");
+			while(true) {
+				int value=new Random().Next(10, 100);
+				string ss=String.Concat("WORKER:", value);
+				byte[] bytes=Encoding.ASCII.GetBytes(ss);
+				sockSend.Send(bytes, 0, bytes.Length);
+				Thread.Sleep(1000);
+			}
 		}
 	}
-	public static void Main(string[] args) {
-		Thread t=new Thread(new ThreadStart(sender));
-		t.Start();
-		using(var context=new ZContext()) using(var subscriber=new ZSocket(context, ZSocketType.SUB)) {
-			Console.WriteLine("Connecting to tcp://127.0.0.1:5557...");
-			subscriber.Connect("tcp://127.0.0.1:5557");
-			subscriber.Subscribe("");
+	public static void receiver(){
+		using(var context=new ZContext()) using(var sockRcpt=new ZSocket(context, ZSocketType.SUB)) {
+			Console.WriteLine("Receiver: tcp://127.0.0.1:5557");
+			sockRcpt.Connect("tcp://127.0.0.1:5557");
+			sockRcpt.Subscribe("");
 			while(true) {
-				using(var replyFrame=subscriber.ReceiveFrame()) {
+				using(var replyFrame=sockRcpt.ReceiveFrame()) {
 					string reply=replyFrame.ReadString();
-					Console.WriteLine(reply);
+					Console.WriteLine("RECEIVED: "+reply);
 				}
 			}
 		}
+	}
+	public static void Main(string[] args) {
+		Thread t=new Thread(new ThreadStart(receiver));
+		t.Start();
+		Thread k=new Thread(new ThreadStart(sender));
+		k.Start();
+		t.Join();
+		k.Join();
 	}
 } }
