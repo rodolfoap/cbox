948c52eca8c7aab1a9028e673b29a4722eb026c5
RodolfoAP <rodolfo.alcazar@gtd.eu>
2020-06-14 02:34:02 +0200

    Changes 2020/06/14-02:34:02
    Change-Id: I8eb55b908429aedc25f6a1e848c03f05ae9133ab

--- 84-0mqVentilatorSink/mono/client.cs
+++ 84-0mqVentilatorSink/mono/client.cs
@@ -2,21 +2,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Threading;
 
 using ZeroMQ;
 
 namespace Examples {
 static partial class Program {
-public static void Main(string[] args) {
-	using(var context=new ZContext()) using(var subscriber=new ZSocket(context, ZSocketType.SUB)) {
-		Console.WriteLine("Connecting to tcp://127.0.0.1:5557...");
-		subscriber.Connect("tcp://127.0.0.1:5557");
-		subscriber.Subscribe("");
+	public static void sender(){
 		while(true) {
-			using(var replyFrame=subscriber.ReceiveFrame()) {
-				string reply=replyFrame.ReadString();
-				Console.WriteLine(reply);
+			Console.WriteLine("T...");
+			Thread.Sleep(1000);
+		}
+	}
+	public static void Main(string[] args) {
+		Thread t=new Thread(new ThreadStart(sender));
+		t.Start();
+		using(var context=new ZContext()) using(var subscriber=new ZSocket(context, ZSocketType.SUB)) {
+			Console.WriteLine("Connecting to tcp://127.0.0.1:5557...");
+			subscriber.Connect("tcp://127.0.0.1:5557");
+			subscriber.Subscribe("");
+			while(true) {
+				using(var replyFrame=subscriber.ReceiveFrame()) {
+					string reply=replyFrame.ReadString();
+					Console.WriteLine(reply);
+				}
 			}
 		}
 	}
-} } }
+} }
