b6c46da497722ae330c1658583a97dad38cf75a6
RodolfoAP <rodolfoap@gmail.com>
2020-06-24 00:00:08 +0200

    Changes 2020/06/24-00:00:08
    Change-Id: I9f4058ff094aef3d03c4b720291441bbacdd71b8

--- 91-std_move/app.cpp
+++ 91-std_move/app.cpp
@@ -1,19 +1,71 @@
 #include <iostream>
-#include <string>
-#define LOG std::cerr<<">>> "<<__FILE__<<"["<<__LINE__<<"]:"<<__func__<<"();"<<std::endl;
-#define log(text)(std::cerr<<text<<std::endl)
+#define log(t) std::cerr<<">>> "<<__FILE__<<"["<<__LINE__<<"]:"<<__func__<<"(): "<<t<<std::endl
+
+struct Number{
+	int* n;
+	Number(){ log("D-CTOR"); }
+	Number(int x){log("P-CTOR"); n=new int; *n=x; }
+	Number(const Number& other):n(other.n) { log("Copy CTOR"); }
+	Number(Number&& other){
+		log("Move CTOR");
+		n=other.n;
+		other.n=nullptr;
+	}
+	Number& operator=(const Number& rhs){
+		n=rhs.n;
+		log("Copy OPTOR");
+		return *this;
+	}
+	~Number(){log("DTOR");}
+	void print(){ if(n)log(*n); else log("NULL");}
+};
 
 int main(){
-	std::string s="Hello, World!";
+	// Calls the default ctor.
+	int d=27;
+	Number d_ctor;
+	d_ctor.n=&d;
+	d_ctor.print();     // 27
 
-	// How does std::move work?
-	// 1. Marks s as an RValue (normally, it's an LValue)
-	// 2. By doing so, it activates the move constructor of m
-	// 3. So, internally, the objects are swapped
-	std::string m=std::move(s);
+	log("----");
 
-	// Verification:
-	std::cerr<<"s is empty: "<<s<<std::endl;
-	std::cerr<<"m contains: "<<m<<std::endl;
+	// Calls the parameter ctor.
+	Number p_ctor{19};
+	p_ctor.print();     // 19
+
+	log("----");
+
+	// Calls the copy ctor.
+	Number copy_ctor=d_ctor;
+	copy_ctor.print();  // 27
+	d_ctor.print();     // 27
+
+	log("----");
+
+	// Calls the move ctor.
+	d_ctor.print();     // 27
+	Number move_ctor1((Number&&)d_ctor);
+	d_ctor.print();     // NULL
+	move_ctor1.print(); // 27
+
+	log("----");
+
+	// Calls the move ctor.
+	p_ctor.print();     // 19
+	Number move_ctor2=std::move(p_ctor);
+	p_ctor.print();     // NULL
+	move_ctor2.print(); // 19
+
+	log("----");
+
+	int c=279;
+	Number copy_optor;  // D-CTOR
+	copy_optor.n=&c;
+	copy_optor.print(); // 279
+	copy_optor=move_ctor2;
+	copy_optor.print(); // 19
+
+	log("----");
+
 	return 0;
 }
