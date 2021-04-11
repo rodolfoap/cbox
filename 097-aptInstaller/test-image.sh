83eed36c4e72c1e21a2d855d2cc2f5a30703c796
RodolfoAP <rodolfo.alcazar@gtd.eu>
2021-04-11 09:45:55 +0200

    Changes 2021/04/11-09:45:55
    Change-Id: I82c1d029e1d78b6ad896b1638a6bcbcbdbe39c52

---
+++ 097-aptInstaller/test-image.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd $(cd $(dirname "${BASH_SOURCE[0]}")&>/dev/null&&pwd)
+. VERSION
+
+set -x
+docker run -ti --rm helloworld:${VERSION} dpkg -L helloworld		|sed 's/^/\t/'
+docker run -ti --rm helloworld:${VERSION} apt-cache show helloworld	|sed 's/^/\t/'
+docker run -ti --rm helloworld:${VERSION} helloworld			|sed 's/^/\t/'
