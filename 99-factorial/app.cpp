35f886552f8465de6732904457581d7d124760f5
RodolfoAP <rodolfo.alcazar@gtd.eu>
2021-12-27 05:05:48 +0100

    Changes 2021/12/27-05:05:48
    Change-Id: Ib9983a69ab9764a3210adac0b9477b01a4a70d2e

---
+++ 99-factorial/app.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int factorial(int n) {
+	if(n == 0) { return 1; }
+	return ( n * factorial(n - 1));
+}
+int main() {
+	int num = 5;
+	std::cout << "Factorial of " << num << " is " << factorial(num) << std::endl;
+	return 0;
+}
