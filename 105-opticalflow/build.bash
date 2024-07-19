c304dfd8ff36cdd0c40e669a03b2cf0833c58e7e
RodolfoAP <rodolfo.alcazar@gtd.eu>
2024-07-19 09:33:37 +0200

    Changes 2024/07/19-09:33:37

---
+++ 105-opticalflow/build.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd $(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+[ -d build/ ] && {
+	pushd build &> /dev/null;
+} || {
+	mkdir build;
+	pushd build &> /dev/null;
+	cmake .. -Wdev;
+}
+make -j8; STATUS=$?
+popd &> /dev/null;
+[ $STATUS == 0 ] && echo [100%] $(ls -l app) || echo [ERROR] Compilation error.
